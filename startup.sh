1120b56382c4d05d96d27816ede0d5ad2c2b09e1
cubikx <31266693+cubikx@users.noreply.github.com>
2026-05-25 11:55:53 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 brave-origin-nightly \
-    --no-sandbox \
     --disable-dev-shm-usage \
     --disable-gpu \
+    --no-first-run \
+    --no-default-browser-check \
+    --disable-brave-welcome \
     --window-size=1280,720 \
     --user-data-dir=/home/kasm-user/.config/BraveSoftware/Brave-Origin-Nightly \
     "$@"
