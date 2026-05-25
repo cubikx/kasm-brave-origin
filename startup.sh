53a10df325d1900e2a5c0baa3fe9200a580ea7dc
cubikx <31266693+cubikx@users.noreply.github.com>
2026-05-25 09:58:54 -0500

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# Launch Brave Browser in Kasm
-brave-browser \
+# Launch Brave Origin Browser in Kasm
+brave-origin \
     --no-sandbox \
     --disable-dev-shm-usage \
     --disable-gpu \
     --window-size=1280,720 \
-    --user-data-dir=/home/kasm-user/.config/BraveSoftware/Brave-Browser \
+    --user-data-dir=/home/kasm-user/.config/BraveSoftware/Brave-Origin \
     "$@"
